61dd435504f7b9aa368925313404b0dddc8d1dbf
Abdelghani Neuhaus <abdelghani.neuhaus@u-bordeaux.fr>
2026-05-21 18:27:48 +0200

    added global stats function

---
+++ asha/src/visualization/count_total_locs_and_clusters.py
@@ -0,0 +1,54 @@
+import os
+import math
+
+from src.io_utils import get_PALMTracer_files, get_poca_files, read_poca_files, read_locPALMTracer_file
+
+# Source - https://stackoverflow.com/a/1392549
+# Posted by monkut, modified by community. See post 'Timeline' for change history
+# Retrieved 2026-05-21, License - CC BY-SA 4.0
+
+
+def convert_size(size_bytes):
+    if size_bytes == 0:
+        return "0B"
+    size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+    i = int(math.floor(math.log(size_bytes, 1024)))
+    p = math.pow(1024, i)
+    s = round(size_bytes / p, 2)
+    return "%s %s" % (s, size_name[i])
+
+def get_size(start_path = '.'):
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(start_path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            # skip if it is symbolic link
+            if not os.path.islink(fp):
+                total_size += os.path.getsize(fp)
+
+    return total_size
+
+print(get_size(), 'bytes')
+
+
+
+def count_total_locs_and_clusters(plate_path):
+    """
+    Count the number of localizations and clusters found after processing the HCS plate.
+
+    Args:
+        plate_path (str): Path to the plate directory.
+
+    Returns:
+        tuple: A tuple containing:
+            - total_localizations (int): Number of localizations in one plate.
+            - total_clusters (int): Number of clusters in one plate.
+    """
+    
+    list_of_pt_files = get_PALMTracer_files(plate_path)
+    list_of_poca_files = get_poca_files(plate_path)
+    total_localizations = sum(len(read_locPALMTracer_file(f)) for f in list_of_pt_files)
+    total_clusters = sum(len(read_poca_files(f)) for f in list_of_poca_files)
+    total_size = convert_size(get_size(plate_path))
+    
+    return total_localizations, total_clusters, total_size
