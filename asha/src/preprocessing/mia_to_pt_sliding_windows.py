d75ae4241b486a88dda2b7d97771c84727504609
Abdelghani Neuhaus <abdelghani.neuhaus@u-bordeaux.fr>
2026-08-17 11:38:07 +0200

    changes

--- asha/src/preprocessing/mia_to_pt_sliding_windows.py
+++ asha/src/preprocessing/mia_to_pt_sliding_windows.py
@@ -26,22 +26,21 @@ def mia_to_pt_sliding_windows(plate_path:str, nlocs:int=60, len_window:int=300,
     
     list_of_mia_files = get_statMIA_files(plate_path)
 
+    total_initial = 0
+    total_filtered_params = 0
+    total_filtered_window = 0
+    total_final = 0
+
     for mia_file in list_of_mia_files:
         raw_data = read_statMIA(mia_file)
         
-        # 1. Comptage initial
         initial_locs = len(raw_data)
-        
+    
         raw_data['Intensity Gauss'] *= 2 * math.pi * raw_data['SigmaX'] * raw_data['SigmaY']
-        
-        # Filtres morphologiques et d'intensité
         raw_data = raw_data[(raw_data['SigmaX'] > sigma_min) & (raw_data['SigmaX'] < sigma_max)]
         raw_data = raw_data[(raw_data['Intensity Gauss'] >= intensity_min) & (raw_data['Intensity Gauss'] <= intensity_max)]
-        
-        # 2. Comptage après les filtres de paramètres
         locs_after_params = len(raw_data)
         filtered_by_params = initial_locs - locs_after_params
-        
         loc_per_frame = raw_data.groupby('Plane').size()
 
         try:
@@ -54,58 +53,62 @@ def mia_to_pt_sliding_windows(plate_path:str, nlocs:int=60, len_window:int=300,
         rolling_avg = loc_per_frame.rolling(window=len_window).mean()
         valid_frames = rolling_avg[rolling_avg < nlocs].index
         
+        locs_after_window = 0
+        filtered_by_window = locs_after_params 
+
         if not valid_frames.empty:
             start_frame = valid_frames[0]
             raw_data = raw_data[(raw_data['Plane'] >= start_frame)]
-            
-            # 3. Comptage après le sliding window
             locs_after_window = len(raw_data)
             filtered_by_window = locs_after_params - locs_after_window
-            
-            # --- AFFICHAGE DES RÉSULTATS ---
-            print(f"\nFichier : {os.path.basename(mia_file)}")
-            print(f"  - Localisations initiales : {initial_locs}")
-            print(f"  - Retirées par les filtres (Sigma/Intensité) : {filtered_by_params}")
-            print(f"  - Retirées par le Sliding Window (frames < {start_frame}) : {filtered_by_window}")
-            print(f"  - Localisations finales conservées : {locs_after_window}")
-            # -------------------------------
-
             loc_per_frame = loc_per_frame[loc_per_frame.index >= start_frame]
             rolling_avg = rolling_avg[rolling_avg.index >= start_frame]
             
-    #         filtered_data = raw_data.rename(columns={'Chi2(Gauss)': 'MSE(Gauss)',
-    #                                                     'Intensity Gauss': 'Integrated_Intensity',
-    #                                                     'CentroidX(pix)': 'CentroidX(px)', 
-    #                                                     'CentroidY(pix)': 'CentroidY(px)',
-    #                                                     'SigmaX': 'SigmaX(px)',
-    #                                                     'SigmaY': 'SigmaY(px)',
-    #                                                     'AngleRad': 'Angle(rad)',
-    #                                                     'MSE(Z)': 'MSE_Z(um)',
-    #                                                     'CentroidZ(µm)': 'CentroidZ(um)'})
+            # filtered_data = raw_data.rename(columns={'Chi2(Gauss)': 'MSE(Gauss)',
+            #                                             'Intensity Gauss': 'Integrated_Intensity',
+            #                                             'CentroidX(pix)': 'CentroidX(px)', 
+            #                                             'CentroidY(pix)': 'CentroidY(px)',
+            #                                             'SigmaX': 'SigmaX(px)',
+            #                                             'SigmaY': 'SigmaY(px)',
+            #                                             'AngleRad': 'Angle(rad)',
+            #                                             'MSE(Z)': 'MSE_Z(um)',
+            #                                             'CentroidZ(µm)': 'CentroidZ(um)'})
             
-    #         filtered_data = filtered_data.drop(columns=['Mean', 'Surface', 'Intensity', 'Perimeter', 'Morpho', 'AngleDeg', 'maxX'])
-    #         filtered_data['id'] = range(1, len(filtered_data) + 1)
-    #         filtered_data['Channel'] = 0.0
-    #         filtered_data = filtered_data[['id', 'Plane', 'Index', 'Channel', 'Integrated_Intensity', 
-    #                                     'CentroidX(px)', 'CentroidY(px)', 'SigmaX(px)', 'SigmaY(px)', 
-    #                                     'Angle(rad)', 'MSE(Gauss)', 'CentroidZ(um)', 'MSE_Z(um)']]
+            # filtered_data = filtered_data.drop(columns=['Mean', 'Surface', 'Intensity', 'Perimeter', 'Morpho', 'AngleDeg', 'maxX'])
+            # filtered_data['id'] = range(1, len(filtered_data) + 1)
+            # filtered_data['Channel'] = 0.0
+            # filtered_data = filtered_data[['id', 'Plane', 'Index', 'Channel', 'Integrated_Intensity', 
+            #                             'CentroidX(px)', 'CentroidY(px)', 'SigmaX(px)', 'SigmaY(px)', 
+            #                             'Angle(rad)', 'MSE(Gauss)', 'CentroidZ(um)', 'MSE_Z(um)']]
 
-    #         meta_data = pd.read_csv(mia_file, nrows=1, sep='\t')
-    #         meta_data = meta_data.rename(columns={'Planes count': 'nb_Planes', 'Points count': 'nb_Points'})
-    #         meta_data = meta_data[['Width', 'Height', 'nb_Planes', 'nb_Points']]
-    #         meta_data['Pixel_Size(um)'] = 0.160
-    #         meta_data['Frame_Duration(s)'] = 0.05
-    #         meta_data['Gaussian_Fit'] = str(None)
-    #         meta_data['Spectral'] = False
-    #         meta_data['nb_Points'] = len(filtered_data)
-    #         output_file = mia_file.replace('statMIA', 'locPALMTracer')
-    #         with open(output_file, 'w', newline='') as file:
-    #             file.write('\t'.join(meta_data.columns) + '\n')
-    #             file.write(meta_data.to_csv(sep='\t', index=False, header=False))
-    #             file.write('\t'.join(filtered_data.columns) + '\n')
-    #             file.write(filtered_data.to_csv(sep='\t', index=False, header=False))
+            # meta_data = pd.read_csv(mia_file, nrows=1, sep='\t')
+            # meta_data = meta_data.rename(columns={'Planes count': 'nb_Planes', 'Points count': 'nb_Points'})
+            # meta_data = meta_data[['Width', 'Height', 'nb_Planes', 'nb_Points']]
+            # meta_data['Pixel_Size(um)'] = 0.160
+            # meta_data['Frame_Duration(s)'] = 0.05
+            # meta_data['Gaussian_Fit'] = str(None)
+            # meta_data['Spectral'] = False
+            # meta_data['nb_Points'] = len(filtered_data)
+            # output_file = mia_file.replace('statMIA', 'locPALMTracer')
+            # with open(output_file, 'w', newline='') as file:
+            #     file.write('\t'.join(meta_data.columns) + '\n')
+            #     file.write(meta_data.to_csv(sep='\t', index=False, header=False))
+            #     file.write('\t'.join(filtered_data.columns) + '\n')
+            #     file.write(filtered_data.to_csv(sep='\t', index=False, header=False))
                 
-    #     else:
-    #         print(f"\nFichier : {os.path.basename(mia_file)} -> AUCUNE FRAME VALIDE TROUVÉE")
+        total_initial += initial_locs
+        total_filtered_params += filtered_by_params
+        total_filtered_window += filtered_by_window
+        total_final += locs_after_window
 
-    # print("\nTerminé.")
+    print("\n" + "="*40)
+    print("RÉSUMÉ GLOBAL DU FILTRAGE")
+    print("="*40)
+    print(f"Total localisations initiales        : {total_initial}")
+    print(f"Total retirées (Sigma/Intensité)     : {total_filtered_params}")
+    print(f"Total retirées (Sliding Window)      : {total_filtered_window}")
+    print("-" * 40)
+    print(f"TOTAL LOCALISATIONS CONSERVÉES       : {total_final}")
+    print("="*40 + "\n")
+    
+    print("Done")
